d9faba5fadb875926978fd0dbf1a16c535502485
wildan <wildansnahar@gmail.com>
2018-05-16 03:51:38 +0000

    Fix to oneline statement

--- once.sh
+++ once.sh
@@ -1,11 +1,2 @@
 !#/bin/sh
-echo 'Starting...';
-# create volume for db
-docker volume create db
-docker volume create contents
-docker volume create assets
-
-# create db container
-docker run -d --name flarum_db_v3 -e MYSQL_ROOT_PASSWORD=wildan123 -e MYSQL_USER=wildan -e MYSQL_PASSWORD=wildan123 MYSQL_DATABASE=flarum -v db:/var/lib/mysql mariadb:10.1
-
-echo 'DB container created';
+echo 'Starting...' && docker volume create db && docker volume create contents && docker volume create assets && docker run -d --name flarum_db_v3 -e MYSQL_ROOT_PASSWORD=wildan123 -e MYSQL_USER=wildan -e MYSQL_PASSWORD=wildan123 -e MYSQL_DATABASE=flarum -v db:/var/lib/mysql mariadb:10.1 && echo 'DB container created';
